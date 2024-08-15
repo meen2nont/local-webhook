8b84d747750a87f8be082874d5d4598300cd8b39
Suphachai Pinhom <suphachai.pinhom@gmail.com>
2024-08-15 19:19:07 +0700

    Update Code

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,33 +1,35 @@
 exports.stripe_webhook = async (req, res) => {
     try {
         // Option check header basic Authorization undecrytion
-        const headerAuth = req.headers.authorization.split(' ')[1]
-
-        // Compare Authorization
-        const yourAuth = 'RzNDMlFkYmN4SktEb3JtOkN4YlNJeFRySEt4cnMyMg=='
-        if (headerAuth !== yourAuth) {
-            const objResponse = {
-                responseCode: '999',
-                responseMesg: 'Authorization failed',
+        let headerAuth = req.headers.authorization
+        if (headerAuth) {
+            headerAuth = req.headers.authorization.split(' ')[1]
+            // Compare Authorization
+            const yourAuth = 'RzNDMlFkYmN4SktEb3JtOkN4YlNJeFRySEt4cnMyMg=='
+            if (headerAuth !== yourAuth) {
+                const objResponse = {
+                    responseCode: '999',
+                    responseMesg: 'Authorization failed',
+                }
+                res.status(200).send(objResponse)
+                return
             }
-            res.status(200).send(objResponse)
-            return
-        } else {
-            // Receive data from API
-            const objData = req.body
-            console.log(objData)
-
-            // Put the data information to use
-
-
-            // Response to API
-            const objResponse = {
-                responseCode: '000',
-                responseMesg: 'Success',
-            }
-            res.status(200).send(objResponse)
-            return
         }
+
+        // Receive data from API
+        const objData = req.body
+        console.log(objData)
+
+        // Put the data information to use
+
+
+        // Response to API
+        const objResponse = {
+            responseCode: '000',
+            responseMesg: 'Success',
+        }
+        res.status(200).send(objResponse)
+        return
 
 
         // // Option check header basic Authorization decrytion user_name and password
